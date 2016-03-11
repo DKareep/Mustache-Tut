fa51e210b24eb09cb484ff5072b652d8afaf9678
Dijin <dijin@lookup.to>
2016-03-11 10:58:26 +0530

    initial commit with base example

---
+++ script.js
@@ -0,0 +1,9 @@
+var person = {
+    firstName: 'Faizan',
+    lastName: 'Mohammad',
+    blogURL: 'http://themeforest.net'
+};
+
+var template = "<h1>{{firstName}} {{lastName}}</h1><h2>Blog: {{blogURL}}</h2>";
+var html = Mustache.to_html(template,person);
+$('#sampleArea').html(html);
